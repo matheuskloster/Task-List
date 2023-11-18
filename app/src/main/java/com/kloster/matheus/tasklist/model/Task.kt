67dd9c2e99729a3e232f038e84d819c75d608f86
matheuskloster <matheuskloster@users.noreply.github.com>
2023-11-18 14:21:51 -0300

    Habilitando View Binding e corrigindo os parametros de task para serem passíveis de alteração

--- app/src/main/java/com/kloster/matheus/tasklist/model/Task.kt
+++ app/src/main/java/com/kloster/matheus/tasklist/model/Task.kt
@@ -2,7 +2,7 @@ package com.kloster.matheus.tasklist.model
 
 private const val TO_DO = "A FAZER"
 
-class Task(name:String, status:String) {
+class Task(var name:String,var status:String) {
     constructor(name: String): this(name, TO_DO)
 
 }
